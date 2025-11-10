5e5121bde719f7ebfdf189f591a6c6032f046aa0
st10275468 <st10275468@vcconnect.edu.za>
2025-11-10 14:35:53 +0200

    feat(Graphs): -Add Category -Connect -GetConnected

--- Municipal Services Portal/Models/Graphs.cs
+++ Municipal Services Portal/Models/Graphs.cs
@@ -2,5 +2,65 @@
 {
     public class Graphs
     {
+        public Dictionary<string, List<string>> connections { get; private set; } = new Dictionary<string, List<string>>();
+
+        public void AddCategory(string category)
+        {
+            if (!connections.ContainsKey(category))
+            {
+                connections[category] = new List<string>();
+            }
+        }
+
+        public void Connect(string start, string end)
+        {
+            AddCategory(start);
+            AddCategory(end);
+            
+            if (!connections[start].Contains(end))
+            {
+                connections[start].Add(end);
+            }
+            if (!connections[end].Contains(start))
+            {
+                connections[end].Add(start);
+            }
+        }
+
+        public List<string> GetConnected(string category) 
+        {
+            List<string> connected = new List<string>();
+            if (!connections.ContainsKey(category))
+            {
+                return connected;
+            }
+
+            Queue<string> queue = new Queue<string>();
+            HashSet<string> visited = new HashSet<string>();
+
+            queue.Enqueue(category);
+            visited.Add(category);
+
+            while (queue.Count > 0)
+            {
+                string current = queue.Dequeue();
+                connected.Add(current);
+
+                foreach (var neighbor in connections[current])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        visited.Add((neighbor));
+                        queue.Enqueue((neighbor));
+                    }
+                }
+            }
+
+            return connected;
+
+        }
+
+
+
     }
 }
